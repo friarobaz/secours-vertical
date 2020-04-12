438e41106cc1c26147b0bc67dd26288f7d2cc4a7
Baptiste Marchand <baptiste.marchand@ambler.fr>
2020-04-12 19:16:47 +0200

    test index generation

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,35 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
 
+  eleventyConfig.addNunjucksFilter("test", function findNavigationEntries(nodes = [], key = "") {
+    let pages = [];
+    for(let entry of nodes) {
+      if(entry.data && entry.data.eleventyNavigation) {
+        let nav = entry.data.eleventyNavigation;
+        if(!key && !nav.parent || nav.parent === key) {
+          pages.push(Object.assign({}, nav, {
+            url: nav.url || entry.data.page.url,
+            pluginType: "eleventy-navigation",
+            entry: entry,
+          }, key ? { parentKey: key } : {}));
+        }
+      }
+    }
+  
+    return pages.sort(function(a, b) {
+      return (a.order || 0) - (b.order || 0);
+    }).map(function(entry) {
+      if(!entry.title) {
+        entry.title = entry.key;
+      }
+      if(entry.key) {
+        entry.children = findNavigationEntries(nodes, entry.key);
+      }
+      return entry;
+    });
+  });
+
+
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   
   // Static assets to pass through
