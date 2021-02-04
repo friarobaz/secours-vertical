6749ffbe7e0dd8c3abf2371e136a7968d10029ef
jules marchand <friarobaz@gmail.com>
2021-02-04 18:32:34 +0100

    boules jaunes

--- src/tirage/script.js
+++ src/tirage/script.js
@@ -1,67 +1,66 @@
 window.addEventListener("load", () =>{
-    
-    const canvas = document.getElementById("myCanvas");
-    const ctx = canvas.getContext("2d");
-    
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    const X = canvas.width;
-    const Y = canvas.height;
-    const NB_OF_QUICKDRAWS = 10;
-    const END_Y = 50;
-    const MIN_X_VARIATION = 10;
-    
-    const MAX_X_VARIATION = X/2/(NB_OF_QUICKDRAWS+2);
-    const Y_INCREMENT = (Y-END_Y) / (NB_OF_QUICKDRAWS+1);
-    const STARTING_POINT = {x:X/2, y:Y};
-    let route = [];
-    
-    function create_route(){
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+/* YOU CAN CHANGE THIS */
+const NB_OF_QUICKDRAWS = 10;
+const END_Y = 50;
+const MIN_X_VARIATION = 10;
+/* ------------------------- */
+
+canvas.width = window.innerWidth; 
+canvas.height = window.innerHeight;
+const MAX_X_VARIATION = canvas.width/2/(NB_OF_QUICKDRAWS+2);
+const Y_INCREMENT = (canvas.height-END_Y) / (NB_OF_QUICKDRAWS+1);
+const STARTING_POINT = {x:canvas.width/2, y:canvas.height};
+let route = [];
+
+function create_route(){
+    let current_point = STARTING_POINT;
+    for (let i = 0; i < NB_OF_QUICKDRAWS+2; i++) {
+        let rand = Math.floor(Math.random() * Math.floor(MAX_X_VARIATION))+MIN_X_VARIATION;
+        let dir = Math.round(Math.random()) * 2 - 1;
         
-        let current_point = STARTING_POINT;
+        let last_point = current_point; //remember current point, keep it in last point
+        route[i] = current_point; //right current point in route
+        
+        //assign next position to current_point 
+        current_point = {x:last_point.x+(rand*dir), y: last_point.y - Y_INCREMENT}; 
+    }//end for 
+} //end function create_route
+
+function draw_route(route){
+    ctx.beginPath();
+    ctx.moveTo(route[0].x, route[0].y);
+    for (let i = 1; i < route.length; i++) {
+        ctx.lineTo(route[i].x, route[i].y);
+    }
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "red";
+    ctx.stroke();
+}//end function draw route
+
+function draw_quickdraws(route){
+    ctx.fillStyle = "yellow";
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "black";
     
-        for (let i = 0; i < NB_OF_QUICKDRAWS+2; i++) {
-            let rand = Math.floor(Math.random() * Math.floor(MAX_X_VARIATION))+MIN_X_VARIATION;
-            let dir = Math.round(Math.random()) * 2 - 1;
-            
-            let last_point = current_point; //remember current point, keep it in last point
-            route[i] = current_point; //right current point in route
-
-            //assign next position to current_point 
-            current_point = {x:last_point.x+(rand*dir), y: last_point.y - Y_INCREMENT}; 
-        }//end for 
-    } //end function create_route
-
-    function draw_route(route){
+    for (let i = 1; i < route.length-1; i++) {
+        //ctx.fillRect(route[i].x, route[i].y, 20, 20)
         ctx.beginPath();
-        ctx.moveTo(route[0].x, route[0].y);
-        for (let i = 0; i < route.length; i++) {
-            ctx.lineTo(route[i].x, route[i].y);
-        }
-        ctx.lineWidth = 5;
-        ctx.strokeStyle = "red";
+        ctx.arc(route[i].x, route[i].y, 10, 0, 2 * Math.PI);
+        ctx.fill();
         ctx.stroke();
     }
+}
 
+for (let i = 0; i < 1; i++) {
     
-    for (let i = 0; i < 1; i++) {
-        
-        create_route();
+    create_route();
     draw_route(route);
-    }
+    draw_quickdraws(route);
+}
     
-});
+});//end everything (window on load)
 
-
-
-
-
-
-
-
-
-
-
-
-/* console.log("penis"); */
 
