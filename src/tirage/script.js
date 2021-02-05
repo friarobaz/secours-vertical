90c8f1856f63d80a5f4681e6b35507d3dc1ca365
jules marchand <friarobaz@gmail.com>
2021-02-05 23:05:44 +0100

    working before algo

--- src/tirage/script.js
+++ src/tirage/script.js
@@ -6,31 +6,30 @@ canvas.height = window.innerHeight;
 
 /* YOU CAN CHANGE THIS */
 const NB_OF_BOLTS = 5;
-const END_Y = 50;
+const END_Y = 20;
 const MIN_X_VARIATION = 10;
-const BOLT_RADIUS = 10;
-const BOLT_COLOR = "yellow";
+const BOLT_RADIUS = 50;
+const BOLT_COLOR = false;
 const START_END_COLOR = "blue";
 const ROPE_COLOR = "green";
 const VECTOR_COLOR = "red";
 const LEVELS = [{rope: true, vectors: true},
                 {rope: true, vectors: false},
                 {rope: false, vectors: false}];
+/* ------------------------- */
 let level = 0;
 let win_streak = 0;
-/* ------------------------- */
-
+let current_route = [];
 const MAX_X_VARIATION = canvas.width/2/(NB_OF_BOLTS+2);
 const Y_INCREMENT = (canvas.height-END_Y) / (NB_OF_BOLTS+1);
 const STARTING_POINT = {x:canvas.width/2, y:canvas.height};
-let route = [];
-let tensions = [];
+
 
 /* --- MATH FUNCTIONS --- */
 function length(a, b){
     return Math.sqrt(Math.pow((a.x - b.x), 2) + Math.pow((a.y - b.y), 2));
 }
-function tension_point(bolt) {
+function tension_point(bolt, route) {
     const AC = {
       x: route[bolt+1].x - route[bolt-1].x,
       y: route[bolt+1].y - route[bolt-1].y
@@ -41,11 +40,12 @@ function tension_point(bolt) {
       y: route[bolt-1].y + k * AC.y
     };
 }
-function tension_strength(bolt){
-    return Math.floor(length(route[bolt], tension_point(bolt)));
+function tension_strength(bolt, route){
+    return Math.floor(length(route[bolt], tension_point(bolt, route)));
 }
 
 function create_route(){
+    let route = [];
     let current_point = STARTING_POINT;
     for (let i = 0; i < NB_OF_BOLTS+2; i++) {
         let rand = Math.floor(Math.random() * MAX_X_VARIATION) + MIN_X_VARIATION;
@@ -57,11 +57,23 @@ function create_route(){
         //assign next position to current_point 
         current_point = {x:last_point.x+(rand*dir), y: last_point.y - Y_INCREMENT}; 
     }//end for 
+    return route;
 } //end function create_route
 
+function smooth(old_route){
+    let better_route = [];
+    better_route[0] = STARTING_POINT; //start
+    better_route[old_route.length-1] = old_route[old_route.length-1]; //finish
+    for (let i = 1; i < old_route.length-1; i++) {
+        better_route[i] = tension_point(i, old_route);
+    }
+    return better_route;
+}
+
 function find_max_tensions(route){
+    let tensions = [];
     for (let i = 1; i < route.length-1; i++) {
-        tensions[i-1] = tension_strength(i);
+        tensions[i-1] = tension_strength(i, route);
     }//end for 
     return tensions.indexOf(Math.max(...tensions))+1;
 }
@@ -70,83 +82,112 @@ function find_max_tensions(route){
 
 function draw_circle (center, radius, strokeColor, strokeWidth, fillColor){  
     ctx.beginPath();
-    ctx.arc(center.x, center.y, radius, 0, 2 * Math.PI);
-    if (fillColor){
-        ctx.fillStyle = fillColor;
+    if (!radius){
+        ctx.arc(center.x, center.y, 5, 0, 2 * Math.PI);
         ctx.fill();
-    }; 
-    if (strokeWidth){
-        ctx.lineWidth = strokeWidth;
-        ctx.strokeStyle = strokeColor;
-        ctx.stroke();
-    };
+    }else{
+        ctx.arc(center.x, center.y, radius, 0, 2 * Math.PI);
+        if (fillColor){
+            ctx.fillStyle = fillColor;
+            ctx.fill();
+        }; 
+        if (strokeWidth){
+            ctx.lineWidth = strokeWidth;
+            ctx.strokeStyle = strokeColor;
+            ctx.stroke();
+        };//end if
+    };//end else
 }//end draw_circle
 
-function draw_line(a,b,couleur, width){
+function draw_line(a,b,color, width){
     ctx.beginPath();
     ctx.moveTo(a.x, a.y);
     ctx.lineTo(b.x, b.y);
-    ctx.lineWidth = width;
-    ctx.strokeStyle = couleur;
+    if (width){ctx.lineWidth = width;}else {ctx.lineWidth = 3;}
+    if (color){ctx.strokeStyle = color;}else {ctx.strokeStyle = "black";}
     ctx.stroke();
 }//end draw_line
 
-function draw_vector (bolt){
-    draw_line(route[bolt], tension_point(bolt), VECTOR_COLOR, 3);
+function draw_path(path, color, width){
+    ctx.beginPath();
+    ctx.moveTo(path[0].x, path[0].y);
+    for (let i = 1; i < path.length; i++) {
+        ctx.lineTo(path[i].x, path[i].y);
+    }
+    if (width){ctx.lineWidth = width;}else {ctx.lineWidth = 3;}
+    if (color){ctx.strokeStyle = color;}else {ctx.strokeStyle = "black";}
+    ctx.stroke();
 }
 
-function draw_bolt (bolt){
+function draw_vector (bolt, route){
+    draw_line(route[bolt], tension_point(bolt, route), VECTOR_COLOR, 3);
+}
+
+function draw_bolt (bolt, route){
     draw_circle(route[bolt], BOLT_RADIUS, "black", 1, BOLT_COLOR);
 }
 
 function draw_route(route, rope, vectors){
     if (rope){
-        ctx.beginPath();
-        ctx.moveTo(route[0].x, route[0].y);
-        for (let i = 1; i < route.length; i++) {
-            ctx.lineTo(route[i].x, route[i].y);
-        }
-        ctx.lineWidth = 3;
-        ctx.strokeStyle = ROPE_COLOR;
-        ctx.stroke();
+        draw_path(route, ROPE_COLOR)
     }//end rope
     for (let i = 1; i < route.length-1; i++) {
-        if (vectors){draw_vector(i)};
-        draw_bolt(i);
+        if (vectors){draw_vector(i, route)};
+        draw_bolt(i, route);
     }
     draw_circle(STARTING_POINT, 10, "black", 1, START_END_COLOR);
     draw_circle(route[route.length-1], 10, "black", 1, START_END_COLOR);
 }//end function draw route
 
-for (let i = 0; i < 1; i++) {
-    create_route();
-    draw_route(route, LEVELS[level].rope, LEVELS[level].vectors);
-    //draw_circle(route[find_max_tensions(route)], 8, "black", 0, "red");
-}//end for
+/* --- END DRAW FUNCTIONS --- */
+
+/* ##################################################################
+############### C'EST ICI QUE CA SE PASSE ########################
+################################################################## */
+
+current_route = create_route();
+draw_route(current_route, LEVELS[level].rope, LEVELS[level].vectors);
+draw_path(smooth(current_route), "pink");
+
+function on_bolt(point, route){
+    for (let i = 1; i < route.length-1; i++) {
+        if(length(point, route[i])< BOLT_RADIUS){
+            //console.log("yes");
+            return i;
+        };
+    }//end for
+    return false;
+}
 
 document.addEventListener("click", function(event){
     let mouse = {};
     mouse.x = event.x;
     mouse.y = event.y
-    for (let i = 1; i < route.length-1; i++) {
-        if(length(mouse, route[i])< BOLT_RADIUS){
-            console.log(i);
-            if(i == find_max_tensions(route)){
-                win_streak++;
+    
+    if (on_bolt(mouse, current_route)){ //if you click on bolt
+        if(on_bolt(mouse, current_route) == find_max_tensions(current_route)){ //if right bolt
+            win_streak++;
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            current_route = create_route();
+            if (level != LEVELS.length-1 && win_streak == 5){
+                level++;
+                win_streak = 0;
+            } else if(level == LEVELS.length-1 && win_streak == 5){
+                alert("Gagné !");
+                level = 0;
+                win_streak = 0;
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
-                create_route();
-                if (level != LEVELS.length-1 && win_streak == 5){
-                    level++;
-                    win_streak = 0;
-                } else if(level == LEVELS.length-1 && win_streak == 5){
-                    alert("Gagné !");
-                }
-                draw_route(route, LEVELS[level].rope, LEVELS[level].vectors);
-            }//end if  
-        }//end if   
-    }//end for
+                current_route = create_route();
+            }
+            draw_route(current_route, LEVELS[level].rope, LEVELS[level].vectors);
+        }//end if 
+    };
 
-  });
+   
+            
+     
+});//end Click
+
 
     
 });//end everything (window on load)
